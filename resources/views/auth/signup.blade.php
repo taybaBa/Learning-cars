6b77148ec73a19bf909b92bb7147ceac6ee00e4f
Tayyba Bashir <taybabashir20@gmail.com>
2025-02-19 20:08:45 +0500

    components and challenge 4:23

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,15 +1,5 @@
-<x-base-layout title="Sign Up" bodyClass="page-signup">
-        <main>
-            <div class="container-small page-login">
-                <div class="flex" style="gap: 5rem">
-                    <div class="auth-page-form">
-                        <div class="text-center">
-                            <a href="/">
-                                <img src="/img/logoipsum-265.svg" alt=""/>
-                            </a>
-                        </div>
+<x-guest-layout title="Sign Up" bodyClass="page-signup">
                         <h1 class="auth-page-title">Signup</h1>
-
                         <form action="" method="post">
                             <div class="form-group">
                                 <input type="email" placeholder="Your Email"/>
@@ -32,21 +22,12 @@
                             </div>
                             <button class="btn btn-primary btn-login w-full">Register</button>
 
-                            <div class="grid grid-cols-2 gap-1 social-auth-buttons">
-                                <x-google-button/>
-                                <x-fb-button title="Facebook"/>
-                            </div>
                             <div class="login-text-dont-have-account">
-                                Already have an account? -
-                                <a href="/login.html"> Click here to login </a>
+                                <x-slot:footerLink>
+                                    Already have an account? -
+                                    <a href="/login.html"> Click here to login </a>
+                                </x-slot:footerLink>
                             </div>
                         </form>
-                    </div>
-                    <div class="auth-page-image">
-                        <img src="/img/car-png-39071.png" alt="" class="img-responsive"/>
-                    </div>
-                </div>
-            </div>
-        </main>
-</x-base-layout>
+</x-guest-layout>
 
